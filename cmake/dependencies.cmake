8210705ea346ec888666efd264fe095f1c0d92c6
Federico Fuga <fuga@studiofuga.com>
2020-11-15 16:25:55 +0100

    Preliminary porting to CMAKE . LibPal + tests ported.
    Signed-off-by: Federico Fuga <fuga@studiofuga.com>

---
+++ cmake/dependencies.cmake
@@ -0,0 +1,7 @@
+set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_SOURCE_DIR}/cmake)
+
+set(THREADS_PREFER_PTHREAD_FLAG ON)
+find_package(Threads REQUIRED)
+add_definitions(-D_HAVE_PTHREAD_)
+
+find_package(GEOS REQUIRED)
