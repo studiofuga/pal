58dcaa6e9f20acce23c91b8e5068f2395c73c9e5
Federico Fuga <fuga@studiofuga.com>
2020-12-28 19:58:00 +0100

    Made GEOS dependency optional
    Signed-off-by: Federico Fuga <fuga@studiofuga.com>

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -4,4 +4,8 @@ set(THREADS_PREFER_PTHREAD_FLAG ON)
 find_package(Threads REQUIRED)
 add_definitions(-D_HAVE_PTHREAD_)
 
-find_package(GEOS REQUIRED)
+find_package(GEOS)
+
+if (GEOS_Found)
+    add_definitions(-DHAVE_GEOS)
+endif()
